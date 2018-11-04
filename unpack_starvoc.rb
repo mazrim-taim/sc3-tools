7f118d8e8679d287247df2435ee780598239ab71
mazrim-taim <todd.pederzani@gmail.com>
2018-11-04 13:34:40 -0500

    Add files via upload
    simple explanation of file format

--- unpack_starvoc.rb
+++ unpack_starvoc.rb
@@ -1,3 +1,17 @@
+# File format for STAR001.VOC included on the Star Control 3 disc
+#   Size: 276,078,592 bytes
+#   Date: ‎August ‎15, ‎1996, ‏‎2:35:38 PM
+#   SHA1: 31d06d53fc9129114aa06e645889e9660e2095e0
+#
+# Header:
+#   2 byte, little-endian: count of following indices
+#   4 byte, little-endian: absolute position from start of file for first chunk
+#   ... repeat above as needed
+# Chunks:
+#   2 byte, little-endian: unknown. only values are 0x2000 and 0x0000
+#   4 byte, little-endian: length of data
+#   following bytes are data (WAV, starting with the "RIFF" header)
+#
 begin
     file = File.open("STAR001.VOC", "rb")
     index = {}
